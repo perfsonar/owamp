baf086202036f3bbeaf1a9bb9675fe767afbd5db
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-04-04 06:24:28 +0000

    Major revamping.
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@20 a2d82226-602c-0410-bba7-e51b83dd10d2

--- Attic/owpcontrib/access.c
+++ Attic/owpcontrib/access.c
@@ -16,21 +16,32 @@
 /* for ntohl */
 #include <sys/param.h>
 
-/*
-** This structure is used to keep track of usage resources.
-*/
-
-struct OWAMPLimits {
-	int fake;
-};
-
-
 #define MAX_LINE 1024
-#define DB_KEY_LEN 128
 
 #ifndef MAXPATHLEN
 #define MAXPATHLEN 1024
 #endif
+
+#define CLASS_NAME 1
+#define LIMIT_NAME 2
+#define NEITHER    3
+
+#define DEBUG 0
+
+struct subnet {
+	u_int32_t network ;
+	u_int8_t offset;
+};
+
+/*
+** This structure is used to keep track of usage resources.
+*/
+
+typedef struct owamp_limits{
+	u_int32_t bandwidth;   /* bytes/sec                          */
+	u_int32_t space;       /* bytes                              */
+	u_int8_t num_sessions; /* number of concurrent test sessions */
+} OWAMPLimits;
 
 const char *DefaultConfigFile = DEFAULT_CONFIG_FILE;
 char *ConfigFile = NULL;
@@ -38,6 +49,7 @@ const char *DefaultIPtoClassFile = DEFAULT_IP_TO_CLASS_FILE;
 char *IPtoClassFile = NULL;
 const char *DefaultClassToLimitsFile = DEFAULT_CLASS_TO_LIMITS_FILE;
 char *ClassToLimitsFile = NULL;
+DBM *ip2class_dbm;
 
 static void usage(void);
 
@@ -45,6 +57,36 @@ static void
 usage()
 {
 	return;
+}
+
+u_int32_t
+OWAMPGetBandwidth(OWAMPLimits * lim){
+	return lim->bandwidth;
+}
+
+u_int32_t
+OWAMPGetSpace(OWAMPLimits * lim){
+	return lim->space;
+}
+
+u_int32_t
+OWAMPGetNumSessions(OWAMPLimits * lim){
+	return lim->num_sessions;
+}
+
+void
+OWAMPSetBandwidth(OWAMPLimits * lim, u_int32_t bw){
+	lim->bandwidth = bw;
+}
+
+u_int32_t
+OWAMPSetSpace(OWAMPLimits * lim, u_int32_t space){
+	lim->space = space;
+}
+
+u_int32_t
+OWAMPSetNumSessions(OWAMPLimits * lim, u_int32_t ns){
+	lim->num_sessions = ns;
 }
 
 /*
@@ -73,11 +115,11 @@ owamp_parse_options(int argc, char *argv[])
     }
 }
 
-
 /*
 ** Turn an IP address in dotted decimal into
 ** a number.  This function also works  for
-** partial addresses.
+** partial addresses. Note that it basically
+** views 255.255.255.255 as an illegal address.
 */
 
 unsigned long
@@ -104,7 +146,7 @@ owamp_numberize(const char *addr)
 ** because the return value is static.
 */
 
-const char *
+char *
 owamp_denumberize(unsigned long addr)
 {
 	static char buffer[INET_ADDRSTRLEN];
@@ -115,28 +157,69 @@ owamp_denumberize(unsigned long addr)
 	return buffer;
 }
 
+
+datum *
+subnet2datum(u_int32_t addr, u_int8_t off)
+{
+	char *bytes;
+	datum *ret;
+	int num_bytes = sizeof(u_int32_t) + sizeof(u_int8_t);
+
+	if ( (ret = (void *)malloc(sizeof(datum))) == NULL)
+		return NULL;
+	if ( (ret->dptr = (void *)malloc(num_bytes)) == NULL)
+		return NULL;
+
+	ret->dsize = num_bytes;
+	*(u_int32_t*)(ret->dptr) = addr;
+	*(u_int8_t*)((ret->dptr) + 4) = off;	
+	return ret;
+}
+
+/*
+** Determines whether the given nework/offset combination
+** is valid. Returns 1 if yes, and 0 otherwise.
+*/
+
+int
+is_valid_network(u_int32_t addr, u_int8_t offset)
+{
+	u_int32_t off_mask;
+
+	if (offset > 32){
+		fprintf(stderr, "offset > 32\n");
+		return 0;
+	}
+	if ((offset == 0) && addr)
+		return 0;
+	off_mask = ((1<<(32-offset)) - 1);
+	return (addr & off_mask)? 0 : 1; 
+}
+
 /*
 ** This function takes a string of the form <address/netmask>
 ** where address is a dot-separated IP address, and netmask
-** is a mask offset. On success it returns the key string of the form
-** "netmask_lower_bound/offset" which uniquely identifies the
-** netmask. Otherwise it returns NULL.
+** is a mask offset. It fills in the location pointed to by dst 
+** with 5 bytes of data: the first four bytes are network address 
+** (host byte prder), and the last byte is the mask offset.
+** It returns 0 on success, and -1 on failure.
 */
 
-char *
-owamp_parse_mask(const char *text)
+
+int
+owamp_parse_mask(const char *text, u_int32_t *addr, u_int8_t *off)
 {
+	
 	char *addr_str = NULL;
 	char *mask_str = NULL;
-	unsigned int lb, off;
-	unsigned long addr, mask, host_bits;
-	char *key;
-
-	key = (void *)malloc(DB_KEY_LEN);
-	if (!key) return NULL;
+	unsigned long mask, host_bits;
 
 	if((addr_str = strdup(text)) == NULL)
-		return NULL;
+		return -1;
+
+
+	if((addr_str = strdup(text)) == NULL)
+		return -1;
 
 	if( strchr(addr_str, '/'))
 	{
@@ -145,62 +228,68 @@ owamp_parse_mask(const char *text)
 		mask_str = strtok(NULL, "/");
 		
 		if(!mask_str)
-			return NULL;
+			return -1;
 		
-		if((addr = owamp_numberize(addr_str)) == -1)
-			return NULL;
+		if((*addr = owamp_numberize(addr_str)) == NULL)
+			return -1;
 	
-		/* CIDR */
-		off = atoi(mask_str);
-		
-		if(off > 32)
-			{
-				/* bad CIDR offset */
-				return NULL;
-			};
-		
-		host_bits = 32 - off;
-		lb = (addr >> host_bits) << host_bits;
-	}
-	else
-	{
-		/* regular IP address */
-		if((addr = owamp_numberize(addr_str)) == -1)
-			return NULL;
-		lb = addr;
-		off = 0;
-	}
+		*off = atoi(mask_str);
+		if(*off > 32)		  /* bad CIDR offset */
+			return -1;
 
-	snprintf(key, DB_KEY_LEN, "%lu/%lu", lb, off);
+	} else {
+		/* regular IP address */
+		if((*addr = owamp_numberize(addr_str)) == NULL)
+			return -1;
+		*off = 0;
+	};
+	
+	if (!is_valid_network(*addr, *off))
+		return -1;
+	
 	free(addr_str); 
-	return key;
+	return 0;
 }
+
+
 
 /*
 ** This function fills out a datum structure with the given string.
 */
 
-int
-owamp_datumify(const char *str, datum *dat)
+datum*
+owamp_datumify(const char *bytes, int dat_len)
 {
-	unsigned int len; 
-	
-	len = strlen(str) + 1;
-	if ( (dat->dptr = (void *)malloc(len)) == NULL) {
-		fprintf(stderr, "warning: malloc failed in owamp_datumify\n");
-		return -1;
-	}
+	datum *dat;
+	if ( (dat = (void *)malloc(sizeof(datum))) == NULL)
+		return NULL;
+	if ( (dat->dptr = (void *)malloc(dat_len)) == NULL) 
+		return NULL;
 
-	strcpy(dat->dptr, str);
-	dat->dsize = len; /* counts the '\0' byte */
-	return 0;
+	bcopy(bytes, dat->dptr, dat_len);
+	dat->dsize = dat_len;
+	return dat;
+}
+
+
+u_int32_t
+get_ip_addr(const datum * dat)
+{
+	return *(u_int32_t*)(dat->dptr);
+}
+
+u_int8_t
+get_offset(const datum * dat)
+{
+	return *(u_int8_t*)((dat->dptr)+4);
 }
 
 /* 
 ** This function reads the configuration file given by the path ip2class, 
 ** processes it and saves the results in Berkeley DB file, with the
 ** same base name, and extension ".db". The database can be printed by the
-** companion function print_dbm().
+** companion function print_dbm(). This function should typically only
+** be called once, outside of any application - to initialize the database.
 */
 
 void
@@ -208,15 +297,19 @@ owamp_read_config(const char *ip2class)
 {
 	char line[MAX_LINE], mask[MAX_LINE], class[MAX_LINE];
 	char err_msg[1024];
-	char *key;
 	FILE *fp;
+	u_int32_t addr;
+	u_int8_t off; 
 	
-	DBM *dbm;
-	datum key_dat, val_dat;
+	datum *key, *val;
+	static const char all[] = "all";
+	char tmp[5];
 
 	/* Open the database file for writing. */
-	if ( (dbm = dbm_open(ip2class, O_CREAT|O_RDWR, 0660)) == NULL){
-		perror("dbopen dbtest for read-write");
+	if ((ip2class_dbm = dbm_open(ip2class, O_CREAT|O_RDWR, 0660)) == NULL){
+		snprintf(err_msg, sizeof(err_msg),
+			 "dbm_open %s.db for writing", ip2class);
+		perror(err_msg);
 		exit(1);
 	}
 
@@ -228,26 +321,40 @@ owamp_read_config(const char *ip2class)
 	}
 
 	while ( (fgets(line, sizeof(line), fp)) != NULL) {
+		int tmp;
+			
+		if (line[0] == '#') 
+			continue;
 		if (sscanf(line, "%s%s", mask, class) != 2) 
 			continue;
-		if ( (key = owamp_parse_mask(mask)) == NULL) 
-			continue;
-
-		/* Now save the key/class pair in a hash. */
-		if ( owamp_datumify(key, &key_dat) < 0 ) continue;
-		if ( owamp_datumify(class, &val_dat) < 0 ) continue;
-		if ( dbm_store(dbm, key_dat, val_dat, DBM_REPLACE) != 0){
-			fprintf(stderr, 
-				"dbm_store failed. key = %s, val = %s\n",
-				key_dat.dptr, val_dat.dptr);
+		if ((tmp = owamp_parse_mask(mask, &addr, &off)) != 0){
+			printf("bad network %s\n", mask);
 			continue;
 		}
+		
+		/* Now save the key/class pair in a hash. */
+		if ( (key = subnet2datum(addr, off)) == NULL)
+			continue;
+
+		if ( (val = owamp_datumify(class, strlen(class)+1)) == NULL )
+			continue;
+
+		if (dbm_store(ip2class_dbm, *key, *val, DBM_REPLACE) != 0)
+			continue;
 	}
 
-	dbm_close(dbm);
+	/* In all cases make sure we have a class "all" for the widest mask. */
+	bzero(tmp, 5);
+	if ((key = owamp_datumify(tmp, 5)) == NULL)
+		goto CLOSE;
+	if ((val = owamp_datumify(all, strlen(all)+1)) == NULL)
+		goto CLOSE;
+	if (dbm_store(ip2class_dbm, *key, *val, DBM_INSERT) != 0)
+		fprintf(stderr, "\ndbm_store failed to insert all key\n\n");
+ CLOSE:
+	dbm_close(ip2class_dbm);
 	return;
 }
-
 
 /*
 ** This function prints out the database, given by the argument <base>.
@@ -262,22 +369,160 @@ owamp_print_dbm(char *base)
 
 	dbm = dbm_open(base, O_RDWR | O_CREAT, 0660);
 	for(key = dbm_firstkey(dbm); key.dptr != NULL; key = dbm_nextkey(dbm)){
+		u_int32_t addr;
+		u_int8_t off;
+		char *valptr;
+
 		val = dbm_fetch(dbm, key);
 		assert(val.dptr);
-		fprintf(stderr, "the value of key %s is = %s\n",
-			key.dptr, val.dptr);
+
+		valptr = (void *)malloc(val.dsize + 1);
+		bcopy(val.dptr, valptr, val.dsize);
+		valptr[val.dsize] = '\0';
+
+		fprintf(stderr, "the value of key %s/%u is = %s\n",
+	       owamp_denumberize(get_ip_addr(&key)), get_offset(&key), valptr);
 	}
 	dbm_close(dbm);
+}
+
+/* 
+** This function reads the configuration file given by the path ip2class, 
+** processes it and saves the results in Berkeley DB file, with the
+** same base name, and extension ".db". The database can be printed by the
+** companion function print_dbm().
+*/
+
+void
+owamp_read_limits(const char *class2limits)
+{
+	char line[MAX_LINE];
+	char err_msg[1024];
+	char *key;
+	FILE *fp;
+	char *cur_class;
+	char *tok;
+	
+	DBM *dbm;
+	datum key_dat, val_dat;
+
+	/* Open the database file for writing. */
+	if ( (dbm = dbm_open(class2limits, O_CREAT|O_RDWR, 0660)) == NULL){
+		snprintf(err_msg, sizeof(err_msg),
+			 "dbm_open %s.db for writing", class2limits);
+		perror(err_msg);
+		exit(1);
+	}
+
+	if ( (fp = fopen(class2limits, "r")) == NULL){
+		snprintf(err_msg, sizeof(err_msg),"fopen %s for reading", 
+			class2limits);
+		perror(err_msg);
+		exit(1);
+	}
+
+	fprintf(stderr, "\n");
+	while ( (fgets(line, sizeof(line), fp)) != NULL) {
+		int len;
+		tok = strtok(line, " \t\n");
+		if (!tok)
+			continue;
+		if (tok[0] == '<'){ /* could be classname */
+			len = strlen(tok);
+			if (len <= 2 || tok[len-1] != '>') 
+				continue;
+			tok[len-1] = '\0'; /* now (line+1) points at class */
+			fprintf(stderr, "DEBUG: class name = %s\n", (tok+1));
+			cur_class = strdup(tok+1);
+			continue;
+		} else {
+			if (!cur_class)
+				continue;
+			tok = strtok(NULL, " \t\n");
+		}
+		
+	}
+}
+
+
+void
+datum2string(const datum * dat, char *str)
+{
+	bcopy(dat->dptr, str, dat->dsize);
+	str[dat->dsize] = '\0';
+}
+
+void
+ipaddr2class(u_int32_t ip, char *class)
+{
+	char key_bytes[5];
+	int offset;
+	char *ascii;
+	datum *key_dat, val_dat;
+	u_int32_t mask_template = 0xFFFFFFFF;
+	
+	for(offset=32; offset>0; offset--){
+		int bits = 32 - offset;
+		u_int32_t mask = (mask_template>>bits)<<bits;
+		key_dat = subnet2datum(ip & mask, offset);
+		ascii = owamp_denumberize(ip & mask);
+		val_dat = dbm_fetch(ip2class_dbm, *key_dat);
+		if (val_dat.dptr){
+			bcopy(val_dat.dptr, class, val_dat.dsize);
+			class[val_dat.dsize] = '\0';
+			return;
+		}
+	}
+
+	/* If not found at this point, assign class "all" */
+	strcpy(class, "all");
 }
 
 int
 main(int argc, char *argv[])
 {
+	char line[MAX_LINE];
+	u_int32_t ip;
+	char key_bytes[5];
+	datum * dat;
+	char class[128];
+	char err_msg[128];
+
 	owamp_parse_options(argc, argv);
 	if (!IPtoClassFile)
 		IPtoClassFile = strdup(DefaultIPtoClassFile);
-	
-	owamp_read_config(IPtoClassFile);
-	owamp_print_dbm(IPtoClassFile);
+	if (!ClassToLimitsFile)
+		ClassToLimitsFile = strdup(DefaultClassToLimitsFile);
+
+	/* Uncomment if wish to read config file. Else will use db on disk */
+	owamp_read_config(IPtoClassFile); 
+
+	owamp_print_dbm(IPtoClassFile); 
+	owamp_read_limits(ClassToLimitsFile);
+
+	if ((ip2class_dbm = dbm_open(IPtoClassFile, O_CREAT|O_RDWR, 0660)) 
+	    == NULL){
+		snprintf(err_msg, sizeof(err_msg),
+			 "dbm_open %s.db for writing", IPtoClassFile);
+		perror(err_msg);
+		exit(1);
+	}
+	while (1){
+		printf("\nEnter a dotted IP address, or 'x' to exit:\n");
+		fgets(line, sizeof(line), stdin);
+		if (line[0] == 'x')
+			exit(0);
+		line[strlen(line)-1] = '\0';
+		
+		if ( (ip = owamp_numberize(line)) == -1){
+			fprintf(stderr,"could not numberize IP = %s\n", line);
+			exit(0);
+		}
+
+		ipaddr2class(ip, class);
+#if 1
+		printf("the class for ip = %lu is %s\n", ip, class);
+#endif	
+	}
 	exit(0);
 }
