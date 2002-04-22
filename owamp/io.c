1908f349a16fa889c657114e07a7f77bc6778276
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-04-22 16:00:42 +0000

    Initial import.
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@79 a2d82226-602c-0410-bba7-e51b83dd10d2

---
+++ owamp/io.c
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <errno.h>
+
+/*
+** Robust low-level IO functions - out of Stevens. Read or write
+** the given number of bytes. Returns -1 on error. No short
+** count is possible.
+*/
+
+ssize_t				       /* Read "n" bytes from a descriptor. */
+readn(int fd, void *vptr, size_t n)
+{
+	size_t	nleft;
+	ssize_t	nread;
+	char	*ptr;
+
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nread = read(fd, ptr, nleft)) < 0) {
+			if (errno == EINTR)
+				nread = 0;	   /* and call read() again */
+			else
+				return(-1);
+		} else if (nread == 0)
+			break;				/* EOF */
+
+		nleft -= nread;
+		ptr   += nread;
+	}
+	return(n - nleft);		/* return >= 0 */
+}
+/* end readn */
+
+ssize_t					/* Write "n" bytes to a descriptor. */
+writen(int fd, const void *vptr, size_t n)
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (errno == EINTR)
+				nwritten = 0;	  /* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+/* end writen */
