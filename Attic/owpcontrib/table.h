f9cd86d50069ef0b36e1d94e1838c8ba575d5981
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-04-23 04:59:42 +0000

    Added some function prototype docs.
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@86 a2d82226-602c-0410-bba7-e51b83dd10d2

--- Attic/owpcontrib/table.h
+++ Attic/owpcontrib/table.h
@@ -25,19 +25,32 @@
 #define T hash_ptr
 typedef struct T *T;
 
+/*
+** This type is used to represent keys and values in a hash.
+*/
 typedef struct {
              char *dptr;
              int dsize;
 } datum;
 
+/*
+** This structure represents a single chain of key/value pairs.
+*/
 struct binding {
 	struct binding *link;
 	const datum *key;
 	datum *value;
 };
 
+/*
+** This type is used to allow customized print-out procedure
+** for keys/values having a specific internal structure.
+*/
 typedef void (*print_binding_func)(const struct binding *p, FILE* fp);
 
+/*
+** These are basic hash-manipulation functions.
+*/
 extern T hash_init(
 		   OWPContext ctx,
 		   int hint,
