462f80396683d6e288b673958433d9f819f5d3af
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-04-26 04:10:40 +0000

    Added _OWPSendServerGreeting(), _OWPReadClientGreeting() and _OWPServerOK().
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@111 a2d82226-602c-0410-bba7-e51b83dd10d2

--- owamp/sprotocol.c
+++ owamp/sprotocol.c
@@ -1,4 +1,3 @@
-
 /*
 **      $Id$
 */
@@ -29,14 +28,22 @@
 */
 #include <owampP.h>
 
+void
+owp_block_decrypt(char *a, char* b, char* c, int d, char* e);
+void
+_OWPServerOK(OWPControl ctrl, u_int8_t code);
+
 int
-_OWPServerSendServerGreeting(
-       OWPControl cntrl,          /* cntrl state structure  */
-       u_int32_t	mode,	  /* modes available        */
-       OWPErrSeverity	*err_ret  /* error - returned       */
-)
+_OWPSendServerGreeting(
+		       OWPControl cntrl,          /* cntrl state structure  */
+		       OWPErrSeverity	*err_ret  /* error - returned       */
+		       )
 {
 	char buf[MAX_MSG];
+	int encrypt = 0;
+	u_int32_t mode = cntrl->mode;	  /* modes available */
+
+	/* first 16 bytes: unused + advertised mode */
 	memset(buf, 0, sizeof(buf));
 	*(int32_t *)(buf + 12) = htonl(mode); /* first 12 bytes unused */
 
@@ -44,5 +51,102 @@ _OWPServerSendServerGreeting(
 	random_bytes(cntrl->challenge, 16);
 	memcpy(buf + 16, cntrl->challenge, 16); /* the last 16 bytes */
 
+	if (send_blocks(cntrl->sockfd, buf, 2, encrypt) < 0){
+		close(cntrl->sockfd);
+		return -1; 
+	}
+	
 	return 0;
+}
+
+int
+_OWPReadClientGreeting(
+		       OWPControl cntrl, 
+		       void* app_data
+		       )
+{
+	u_int32_t mode_offered = cntrl->mode;
+	u_int32_t mode_requested; 
+	char buf[MAX_MSG];
+	OWPErrSeverity err_ret;
+	char token[32];
+
+	if (readn(cntrl->sockfd, buf, 60) != 60){
+		close(cntrl->sockfd);
+		return -1;
+	}
+
+	mode_requested = ntohl(*(u_int32_t *)buf);
+	
+	/*
+	     XXX - TODO: improve logic of handling mode_requested.
+	*/
+
+	if (mode_requested & ~mode_offered){ /* can't provide requested mode */
+		_OWPServerOK(cntrl, CTRL_REJECT);
+		close(cntrl->sockfd);
+		return -1;
+	}
+	
+	if (mode_requested & OWP_MODE_AUTHENTICATED){
+		OWPByte *ptr = cntrl->key;
+
+		memcpy(cntrl->kid, buf + 4, 8); /* Save 8 bytes of kid */
+
+		(*cntrl->ctx->cfg.get_aes_key_func)(app_data, buf + 4, 
+					       &ptr, &err_ret);
+		/* if all ok - set up key + cipher now 
+		     XXX - TODO
+		*/
+
+		/* Decrypt two 16-byte blocks - save the result into token.*/
+		owp_block_decrypt(cntrl->readIV, cntrl->key, 
+				  buf + 12, 2*(16*8), token);
+		
+		/* Decrypted challenge is in the first 16 bytes */
+		if (memcmp(cntrl->challenge, token, 16) != 0){
+			_OWPServerOK(cntrl, CTRL_REJECT);
+			close(cntrl->sockfd);
+			return -1;
+		}
+
+		/* Save 16 bytes of session key and 16 bytes of client IV*/
+		memcpy(cntrl->session_key, token + 16, 16);
+		memcpy(cntrl->readIV, buf + 44, 16);
+	}
+
+	/* Apparently everything is ok. Accept the Control session. */
+	cntrl->mode = mode_requested;
+	_OWPServerOK(cntrl, CTRL_ACCEPT);
+	return 0;
+
+	/*
+	      XXX - TODO: make sure all fields off cntrl are set.
+	*/
+}
+
+/*
+** Accept or reject the Control Connection request.
+** Code = CTRL_ACCEPT/CTRL_REJECT with the obvious meaning.
+*/
+void
+_OWPServerOK(OWPControl ctrl, u_int8_t code)
+{
+	char buf[MAX_MSG];
+
+	memset(buf, 0, 32);
+	*(u_int8_t *)(buf+15) = code;
+	if (code == 0){ /* accept */
+		random_bytes(buf + 16, 16); /* Generate Server-IV */
+		/* XXX - TODO
+		   OWPSetWriteIV(ctrl, buf + 16); 
+		*/
+	}
+	send_blocks(ctrl->sockfd, buf, 2, 0);
+}
+
+void
+owp_block_decrypt(char *a, char* b, char* c, int d, char* e)
+{
+	;
 }
