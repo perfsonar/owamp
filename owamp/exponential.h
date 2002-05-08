433cba30809e56403daa76cbb07ddb9027de381e
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-05-08 22:14:44 +0000

    Implementation of uniform and exponential distributions following the original Marsaglia-Ahrens (1972) algorithm as described in Knuth (1998), with latest improvements. Culled from the libranlib C library for random number generators.
    Usage: float genexp(float mu);
    
    will generate successive draws from exponential distribution
    with mean mu. (I've just generated 200 of those and visually it
    looks good).
    
    Current implementation is using made on-the-spot uniform draws.
    Alternative is to use system-provided random() to get the
    uniform deviates as input for the algorithm.
    
    
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@142 a2d82226-602c-0410-bba7-e51b83dd10d2

---
+++ owamp/exponential.h
@@ -0,0 +1,11 @@
+/* Prototypes for all user accessible RANLIB routines */
+
+extern float genexp(float av);
+extern void gscgn(long getset,long *g);
+extern long ignlgi(void);
+extern float ranf(void);
+extern void setsd(long iseed1,long iseed2);
+extern float sexpo(void);
+extern void setall(long iseed1,long iseed2);
+extern void initgn(long isdtyp);
+extern long mltmod(long a,long s,long m);
