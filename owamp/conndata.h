93c154662e15c256ec09feee7589592b3138d82d
boote <boote@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-07-04 16:46:19 +0000

    Adding.
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@245 a2d82226-602c-0410-bba7-e51b83dd10d2

---
+++ owamp/conndata.h
@@ -0,0 +1,35 @@
+/*
+ *      $Id$
+ */
+/************************************************************************
+*									*
+*			     Copyright (C)  2002			*
+*				Internet2				*
+*			     All Rights Reserved			*
+*									*
+************************************************************************/
+/*
+ *	File:		conndata.h
+ *
+ *	Author:		Jeff W. Boote
+ *			Internet2
+ *
+ *	Date:		Thu Jul 04 10:24:44 MDT 2002
+ *
+ *	Description:	
+ *			This file declares the datatype used by the
+ *			default connection related functions to maintain
+ *			state. (pipefd to parent process for requesting
+ *			resources, and holds paths etc. to determine
+ *			where to store session files.)
+ */
+#ifndef	_OWP_CONNDATA_H
+#define	_OWP_CONNDATA_H
+
+typedef struct OWPPerConnDataRec{
+	int	pipefd;		/* pipe to parent	*/
+
+	char	*session_data_path;
+} OWPPerConnDataRec, OWPPerConnData;
+
+#endif	/*	_OWP_CONNDATA_H	*/
