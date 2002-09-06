b7c223bab03a8a4611f6f44df5b2d0e5afa16d10
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-09-06 20:13:40 +0000

    The script now takes in a dirname and loops forever - matching '*.owp' files and trying to send them off (round-robin so as not to inflict starvation). Successfully transferred files are deleted (after md5 check, of course)
    Usage: nodemaster.pl [dirname]
    
    --Tolya
    
    
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@403 a2d82226-602c-0410-bba7-e51b83dd10d2

--- scripts/nodemaster.pl
+++ scripts/nodemaster.pl
@@ -4,10 +4,12 @@
 # Author: Anatoly Karp, Internet2 2002
 # $Id$
 
-# This script manages uploading new data files to the central host (also
-# referred to as remote host) and checks their integrity using md5 hash.
+# This script manages uploading new data files from a given directory 
+# (or current directory, if omitted) to the central host (also referred to
+# as remote host) and checks their integrity using md5 hash.
+# Read configuration section to fine-tune scripts behaviour.
 
-# Usage: nodemaster.pl <filename>
+# Usage: nodemaster.pl [dirname]
 
 use strict;
 use constant DEBUG => 1;
@@ -19,6 +21,8 @@ my $remote_md5_path = '/usr/bin/md5sum';    # same on the remote host
 my $remote_user = 'karp\@sss.advanced.org'; # argument to scp
 my $remote_dir = 'datadir';                 # remote dir to place the files
                                             # NOTE: make sure it exists!
+
+my $data_glob = '*.owp';                    # shell glob to match data files
 
 # due to variety of md5 programs, the fields in which the md5 is found
 # may vary. For example', FreeBSD's output of md5 program may look so:
@@ -36,13 +40,35 @@ my $remote_md5_field = 0;
 
 ### End of configuration section.
 
-my $filename = $ARGV[0];           # datafile to be transferred to remote host
-chomp $filename;
-my $md5_string = qx/$local_md5_path $filename/;
-my @res = split(' ', $md5_string);
-chomp $res[$local_md5_field];
-until (push_ok($res[$local_md5_field], $remote_dir, $filename)) {
-  ;
+my $dirname = $ARGV[0] || '.';
+chdir $dirname or die "could not chdir: $!";
+
+while (1) {  # look for matching files and send them off - round-robin style
+  my $files = qx/ls $data_glob/;
+
+  unless ($files) {
+    warn "no matching filenames found";
+    die "DEBUG" if 1;               # XXX - comment out eventually
+    sleep 60;
+    next;
+  }
+
+  print $files if DEBUG;
+
+  my @files = split /\s/, $files;
+  foreach my $file (@files) {
+    push_try($file);
+  }
+}
+
+# this sub attempts to transfer the file to the remote host
+# and deletes it on successf
+sub push_try {
+  my $filename = $_[0];             # datafile to be transferred to remote host
+  my $md5_string = qx/$local_md5_path $filename/;
+  my @res = split(' ', $md5_string);
+  chomp $res[$local_md5_field];
+  unlink $filename if push_ok($res[$local_md5_field], $remote_dir, $filename);
 }
 
 # try to scp a given file to the remote host and check its md5 value
@@ -53,8 +79,9 @@ sub push_ok {
   my $cmd = join(' ', 'scp', $file, "$remote_user:$rem_dir", '>/dev/null');
   print "DEBUG: local md5 = $md5_loc" if DEBUG;
   print "DEBUG: cmd = ", $cmd if DEBUG;
-  if (system($cmd) < 0) {
+  if (system($cmd) > 0) {
     warn "system() failed: $!";
+    return undef;
   }
   
   my $relative_filename = $file;
@@ -62,15 +89,15 @@ sub push_ok {
     $relative_filename = "$rem_dir/$file";
   }
 
-  print "DEBUG: relative filename = ", $relative_filename if DEBUG;
+  print "DEBUG: remote relative filename = ", $relative_filename if DEBUG;
 
   my $out = qx/ssh $remote_user $remote_md5_path $relative_filename/;
   return undef unless $out;
 
-  my @res = split(' ', $out);
+  my @res = split /\s/, $out;
 
   print "DEBUG: remote md5 = $res[$remote_md5_field]" if DEBUG;
-  chomp $res[0];
+  print "" if DEBUG;
 
   return ($md5_loc eq $res[$remote_md5_field])? 1 : undef;
 }
