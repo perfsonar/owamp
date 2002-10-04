a69aee42fb635e4938f709def85e04021b9f7102
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-10-04 22:41:19 +0000

    Cleaned up and plugged in all conf variables.
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@481 a2d82226-602c-0410-bba7-e51b83dd10d2

--- scripts/livenotify.pl
+++ scripts/livenotify.pl
@@ -13,7 +13,7 @@
 # Usage: livenotify.pl
 
 use strict;
-use constant JAN_1970 => 0x83aa7e80; # offset in seconds
+# use constant JAN_1970 => 0x83aa7e80; # offset in seconds
 use constant TMP_SECRET => 'abcdefgh12345678';
 use Math::BigInt;
 use IO::Socket;
@@ -23,102 +23,79 @@ use OWP;
 use Digest::MD5;
 use GDBM_File;
 
-### Start of the configuration section. Change these values as appropriate.
 use constant VERBOSE => 2;
 use constant DEBUG => 1;  # XXX - eventually set to 0 for production
 use constant NUM_INTERVALS => 10; # max number of intervals in liveness updates
 
-# ASCII address of the local node - to be used as directory
-# on the central host
-die "Usage:$FindBin::Script addr confdir" if($#ARGV != 2);
-my $local_addr = $ARGV[0];
-my $conf = new OWP::Conf($local_addr, $ARGV[1]);
-
-# my $remote_host = 'marsalis.internet2.edu';
-my $remote_host = $conf->{'UPTIMESENDTOADDR'};
-
-my $port = $conf->{'UPTIMESENDTOPORT'};
-
-# this local dir contains the subtree of senders to the local host
-my $local_top = $conf->{'NODEDATADIR'};
-
-# remote dir to place the files. NOTE: make sure it exists!
-# my $remote_top = 'owp/owamp/scripts';	
-my $remote_top = $conf->{'CENTRALUPLOADDIR'};
+my $conf = new OWP::Conf(CONFDIR => "$FindBin::Bin");
 
 # this directory contains owampd configuration files describes below.
 my $owampd_confdir = $conf->{'OWAMPDVARPATH'};
+my $remote_host = $conf->{'UPTIMESENDTOADDR'};
+my $port = $conf->{'UPTIMESENDTOPORT'};
+my $local_node = $conf->get_val(ATTR => 'NODE');
 
 # These files are relative to $owampd_confdir::
-my $owampd_pid_file = 'owampd.pid';    # file containing owampd pid
-my $owampd_info_file = 'owampd.info';  # file containing starttime
+my $owampd_pid_file = $conf->{'OWAMPDPIDFILE'};   # file containing owampd pid
+my $owampd_info_file = $conf->{'OWAMPDINFOFILE'}; # file containing starttime
+
+# this file keeps the last NUM_INTERVALS live time intervals
+my $log_file = $conf->{'UPTIME_DB'};
+my $var_path = $conf->{'OWAMPDVARPATH'};
+
+my $secret = $conf->{'SECRETNAME'};
 
 # separator of items in update messages
 my $update_sep = '_';
 
-# this file keeps the last NUM_INTERVALS live time intervals
-my $log_file = "$top_dir/liveness.dat";
-
-### End of configuration section.
-
-chdir $local_top or die "could not chdir $local_top: $!";
-
-
-unless (defined($conf->{'SECRETNAME'}) &&
-				defined($conf->{$conf->{'SECRETNAME'}})){
-    warn "no secret found in $conf->{'GLOBALCONF'} - using a fake one";
-    $secret = TMP_SECRET;
-}
-$secret = $conf->{$conf->{'SECRETNAME'}};
+chdir $conf->{'DATADIR'} or die "could not chdir $conf->{'DATADIR'} $!";
 
 # Read owampd start time.
 my $info_path = "$owampd_confdir/$owampd_info_file";
 open INFO, "<$info_path" or die "Could not open $info_path: $!";
 my $starttime = <INFO>;
 die "Could not find start time in $info_path" unless $starttime;
-chomp $starttime;
 close INFO;
+chomp $starttime;
 
 # Read owampd pid.
 my $pid_path = "$owampd_confdir/$owampd_pid_file";
 open PID_FILE, "<$pid_path" or die
 	"could not open $pid_path: $!";
 my $pid = <PID_FILE>;
-close PID_FILE;
 die "Could not find pid in $pid_path" unless $pid;
+close PID_FILE;
 chomp $pid;
 
-my $offset_1970 = new Math::BigInt JAN_1970;
-my $scale = new Math::BigInt 2**32;
+# my $offset_1970 = new Math::BigInt JAN_1970;
+# my $scale = new Math::BigInt 2**32;
 
 # Open the database and read in its contents. Currently only one key.
-tie my %state, 'GDBM_File', $log_file, &GDBM_WRCREAT, 0640;
+tie my %state, 'GDBM_File', "$var_path/$log_file", &GDBM_WRCREAT, 0640;
 my $start = time;
+my @live_int = ();
 if (defined $state{'live_times'}) {
 
     # these are just unscaled return values from `time'
-    my @intervals = split /$update_sep/, $state{'live_times'};
+    @live_int = split /$update_sep/, $state{'live_times'};
 
     # If reached the limit then just shift off one pair. This way code
     # in the main loop only needs to change the last interval.
-    if (@intervals == 2*NUM_INTERVALS) {
-	shift @intervals;
-	shift @intervals;
+    if (@live_int == 2*NUM_INTERVALS) {
+	shift @live_int;
+	shift @live_int;
     }
-    push @intervals, $start, time();
-    $state{'live_times'} = join($update_sep, @intervals);
-} else {
-    $state{'live_times'} = join($update_sep, $start, time());
 }
+
+# Update the list of live times.
+push @live_int, $start, time();
+$state{'live_times'} = join($update_sep, @live_int);
 
 socket(my $socket, PF_INET, SOCK_DGRAM, getprotobyname('udp'))
 	or die "socket: $!";
 my $remote_addr = sockaddr_in($port, inet_aton($remote_host));
 connect $socket, $remote_addr 
 	or die "Could not connect to $remote_host:$port : $!";
-
-print "transferring to $remote_user:$remote_top\n" if VERBOSE;
-# die "DEBUG set - exiting" if DEBUG;
 
 while (1) {
 
@@ -132,25 +109,27 @@ while (1) {
     # Update the database.
     my $curtime = time;
     my @intervals = split /$update_sep/, $state{'live_times'};
+
     $intervals[$#intervals] = $curtime;
+    print "intervals: ", join " ", @intervals, "\n";
+
     $state{'live_times'} = join($update_sep, @intervals);
 
-    my @owp_intervals = map(time2owptime, @intervals);
+    print "intervals=$state{'live_times'}\n";
+
+    my @owp_intervals = ();
+    for (@intervals) {
+	push @owp_intervals, OWP::Utils::time2owptime($_);
+    }
 
     # Hash the message with the secret key and send it
-    my $msg = join($sep, $local_node, @owp_intervals);
-    my $plain = join($sep, $msg, $secret);
+    my $msg = join($update_sep, $local_node, @owp_intervals);
+    my $plain = join($update_sep, $msg, $secret);
     my $hashed = Digest::MD5::md5_hex($plain);
-    my $msg_for_send = join($sep, $hashed, $msg);
+    my $msg_for_send = join($update_sep, $hashed, $msg);
     warn "sending ", $msg_for_send, "\n\n" if VERBOSE;
     send $socket, $msg_for_send, 0;
+
+    sleep 2;
 }
 
-# Convert value return by time() into owamp-style (ASCII form
-# of the unsigned 64-bit integer [32.32]
-sub time2owptime {
-    my $bigtime = new Math::BigInt $_[0];
-    $bigtime = ($bigtime + $offset_1970) * $scale;
-    $bigtime =~ s/^\+//;
-    return bigtime;
-}
