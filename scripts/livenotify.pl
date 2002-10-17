e6315dd11a1326ecc6cfae8204af2bf20eeb2bdd
karp <karp@a2d82226-602c-0410-bba7-e51b83dd10d2>
2002-10-17 23:16:12 +0000

    Improved robustness. Factored out repeated code (fetching starttime/pid).
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@508 a2d82226-602c-0410-bba7-e51b83dd10d2

--- scripts/livenotify.pl
+++ scripts/livenotify.pl
@@ -24,7 +24,7 @@ use Digest::MD5;
 use GDBM_File;
 
 use constant VERBOSE => 2;
-use constant DEBUG => 1;  # XXX - eventually set to 0 for production
+use constant DEBUG => 0;  # XXX - eventually set to 0 for production
 use constant NUM_INTERVALS => 10; # max number of intervals in liveness updates
 
 my $conf = new OWP::Conf(CONFDIR => "$FindBin::Bin");
@@ -46,37 +46,18 @@ my $var_path = $conf->{'OWAMPDVARPATH'};
 my $secret = $conf->{'SECRETNAME'};
 
 # separator of items in update messages
-my $update_sep = '_';
+my $update_sep = $conf->must_get_val(ATTR => 'SEPARATOR');
 
 chdir $conf->{'DATADIR'} or die "could not chdir $conf->{'DATADIR'} $!";
 
-# Read owampd start time.
-my $info_path = "$owampd_confdir/$owampd_info_file";
-open INFO, "<$info_path" or die "Could not open $info_path: $!";
-my $starttime = <INFO>;
-die "Could not find start time in $info_path" unless $starttime;
-close INFO;
-chomp $starttime;
-
-# Read owampd pid.
-my $pid_path = "$owampd_confdir/$owampd_pid_file";
-open PID_FILE, "<$pid_path" or die
-	"could not open $pid_path: $!";
-my $pid = <PID_FILE>;
-die "Could not find pid in $pid_path" unless $pid;
-close PID_FILE;
-chomp $pid;
-
-# my $offset_1970 = new Math::BigInt JAN_1970;
-# my $scale = new Math::BigInt 2**32;
-
 # Open the database and read in its contents. Currently only one key.
 tie my %state, 'GDBM_File', "$var_path/$log_file", &GDBM_WRCREAT, 0640;
-my $start = time;
+
+# my $start = time;
 my @live_int = ();
 if (defined $state{'live_times'}) {
 
-    # these are just unscaled return values from `time'
+    # these are in owp format.
     @live_int = split /$update_sep/, $state{'live_times'};
 
     # If reached the limit then just shift off one pair. This way code
@@ -88,8 +69,12 @@ if (defined $state{'live_times'}) {
 }
 
 # Update the list of live times.
-push @live_int, $start, time();
-$state{'live_times'} = join($update_sep, @live_int);
+my ($starttime, $pid) = get_owp_info("$owampd_confdir/$owampd_info_file",
+				     "$owampd_confdir/$owampd_pid_file");
+if (@live_int == 0 || $live_int[-2] ne $starttime) {
+    push @live_int, $starttime, OWP::Utils::time2owptime(time);
+    $state{'live_times'} = join($update_sep, @live_int);
+}
 
 socket(my $socket, PF_INET, SOCK_DGRAM, getprotobyname('udp'))
 	or die "socket: $!";
@@ -97,39 +82,61 @@ my $remote_addr = sockaddr_in($port, inet_aton($remote_host));
 connect $socket, $remote_addr 
 	or die "Could not connect to $remote_host:$port : $!";
 
+# XXX - May need to make this configurable.
+my $sleeptime = 5;
+
 while (1) {
 
     if (kill(0, $pid) != 1) {
 	warn "could not signal pid $pid: $!";
-	die "DEBUG set - exiting" if DEBUG;
-	next;
-    }
-    print "owampd process $pid alive...\n" if VERBOSE;
+	die "DEBUG set - exiting" if 0;
 
+	sleep $sleeptime;
+	($starttime, $pid) = get_owp_info("$owampd_confdir/$owampd_info_file",
+					  "$owampd_confdir/$owampd_pid_file");
+	next;
+     }
     # Update the database.
-    my $curtime = time;
     my @intervals = split /$update_sep/, $state{'live_times'};
 
+    my $curtime = OWP::Utils::time2owptime(time);
     $intervals[$#intervals] = $curtime;
-    print "intervals: ", join " ", @intervals, "\n";
+
+    print "intervals: ", join " ", @intervals, "\n" if DEBUG;
 
     $state{'live_times'} = join($update_sep, @intervals);
 
-    print "intervals=$state{'live_times'}\n";
-
-    my @owp_intervals = ();
-    for (@intervals) {
-	push @owp_intervals, OWP::Utils::time2owptime($_);
-    }
-
     # Hash the message with the secret key and send it
-    my $msg = join($update_sep, $local_node, @owp_intervals);
+    my $msg = join($update_sep, $local_node, @intervals);
     my $plain = join($update_sep, $msg, $secret);
     my $hashed = Digest::MD5::md5_hex($plain);
     my $msg_for_send = join($update_sep, $hashed, $msg);
     warn "sending ", $msg_for_send, "\n\n" if VERBOSE;
     send $socket, $msg_for_send, 0;
 
-    sleep 2;
+    sleep $sleeptime;
 }
 
+
+# Fetch owampd starttime and pid.
+sub get_owp_info {
+    my ($infofile, $pidfile) = @_;
+    my ($starttime, $pid);
+
+    # Read owampd start time.
+    open INFO, "<$infofile" or die "Could not open $infofile: $!";
+    $starttime = <INFO>;
+    die "Could not find start time in $infofile" unless $starttime;
+    close INFO;
+    chomp $starttime;
+
+    # Read owampd pid.
+    open PID_FILE, "<$pidfile" or die
+	    "could not open $pidfile: $!";
+    $pid = <PID_FILE>;
+    die "Could not find pid in $pidfile" unless $pid;
+    close PID_FILE;
+    chomp $pid;
+
+    return ($starttime, $pid)
+}
