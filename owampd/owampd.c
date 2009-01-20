4ca1ce692347c4043ffdd4ae92a6a2eb2acc4ab7
aaron <aaron@a2d82226-602c-0410-bba7-e51b83dd10d2>
2009-01-20 13:01:46 +0000

    Truncate the PID file before printing the PID to it. This fixes an issue where a high PID process would get restarted and the new instance would have a low PID. The pid file would then look like:
    283
    8
    
    This would not work all that well.
    
    
    
    git-svn-id: https://svn.internet2.edu/svn/owamp/trunk@1045 a2d82226-602c-0410-bba7-e51b83dd10d2

--- owampd/owampd.c
+++ owampd/owampd.c
@@ -1583,6 +1583,7 @@ int main(
     if(mypid > 0){
 
         /* Record pid.  */
+        ftruncate(pid_fd, 0);
         fprintf(pid_fp, "%lld\n", (long long)mypid);
         if (fflush(pid_fp) < 0) {
             I2ErrLogP(errhand, errno, "fflush: %M");
